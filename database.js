f8b2787db39d95eacb5d15ee0cc2751b8f632ff5
Maria T. Andersen <mteilmann93@gmail.com>
2023-07-30 17:48:30 +0200

    Initialize a database and ignore the created database file, db.sqlite

---
+++ database.js
@@ -0,0 +1,28 @@
+const sqlite3 = require("sqlite3").verbose();
+const DBSOURCE = "db.sqlite";
+
+let db = new sqlite3.Database(DBSOURCE, (err) => {
+  if (err) {
+    // Cannot open database
+    console.error(err.message);
+    throw err;
+  } else {
+    console.log("Connected to the SQLite database.");
+    db.run(
+      `CREATE TABLE stockSummary (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name text, 
+            price text,
+            )`,
+      (err) => {
+        if (err) {
+          console.log("Table already created.");
+        } else {
+          console.log("Table just created.");
+        }
+      }
+    );
+  }
+});
+
+module.exports = db;
